d7a57e95d5fc2bfc9eb9fb1d324d9ece5e42a92f
tuantinghuang <th2703@nyu.edu>
2023-04-09 13:18:11 -0400

    fix server

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-// app.get('/', function (req, res) {
-//     res.sendFile(path.join(__dirname, '/public/'));
-// });
-
-app.use('/', express.static('public'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join('public', '/index.html'));
+});
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
