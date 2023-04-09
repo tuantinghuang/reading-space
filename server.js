742c9e2dc9e342fecba55daf855700b8530b7690
tuantinghuang <th2703@nyu.edu>
2023-04-09 13:33:08 -0400

    fixing server

--- server.js
+++ server.js
@@ -4,10 +4,9 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-// sendFile will go here
-app.get('/', function (req, res) {
-    res.sendFile(path.join('public', '/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(__dirname)
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
